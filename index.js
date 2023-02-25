03064919abb93adc09d1ab0b177f23fa0c44f0ff
Yusufmert3444 <79647549+Yusufmert3444@users.noreply.github.com>
2023-02-25 14:24:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Text, View,StyleSheet } from 'react-native';
+import styles from './styles';
+
+const Welcome = () =>{
+    return(
+        <View>
+            <Text style={styles.travelytext}>Travely</Text>
+        </View>
+    )
+}
+
+export default Welcome;
---
+++ styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    travelytext:{
+        alignContent : 'center',
+        marginTop: '100px',
+        fontWeight: 'bold',
+    }
+});
+
+export default styles;
